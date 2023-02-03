831e75bff6f511bb947bf180ff772f7d6a2e66e5
root <root@ALMAVIA389-W10.almavia.fr>
2023-02-03 09:47:17 +0100

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'platformsh/starter',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'platformsh/config-reader' => array(
+            'pretty_version' => '2.4.0',
+            'version' => '2.4.0.0',
+            'reference' => '5511abfdb673ccfcf0eac9eaf65204f73edd90fd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../platformsh/config-reader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'platformsh/starter' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
